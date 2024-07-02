3c2fbe593e579d6098e308df0eb818ae2775049f
maaz-afridi20 <maazafridi.ma47@gmail.com>
2024-07-02 21:03:18 +0500

    sdfa

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Instagram',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const Scaffold());
+  }
+}
